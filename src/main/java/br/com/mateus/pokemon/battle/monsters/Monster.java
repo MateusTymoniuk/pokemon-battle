37e426f055cef98b06df1e08ba11686db5a2164b
Mateus Tymoniuk <tymoniuk.m@gmail.com>
2020-08-18 20:58:19 -0300

    Remoção do lombok - Removendo lombok da entidade de montros pois ele causava um bug e não criava um novo item - Removendo lombok do pom

--- src/main/java/br/com/mateus/pokemon/battle/monsters/Monster.java
+++ src/main/java/br/com/mateus/pokemon/battle/monsters/Monster.java
@@ -1,14 +1,11 @@
 package br.com.mateus.pokemon.battle.monsters;
 
-import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-@Data
 public class Monster {
 
     @Id
@@ -35,4 +32,35 @@ public class Monster {
         this.level = level;
     }
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getStrength() {
+		return strength;
+	}
+
+	public void setStrength(Double strength) {
+		this.strength = strength;
+	}
+
+	public Integer getLevel() {
+		return level;
+	}
+
+	public void setLevel(Integer level) {
+		this.level = level;
+	}
 }
